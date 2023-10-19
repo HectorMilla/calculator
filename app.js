289c4350782091f1379490b5954523b3bbf88514
Hector Milla <37377168+HectorMilla@users.noreply.github.com>
2023-10-19 18:35:18 -0400

    added function to run operate func when = button is clicked

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let firstNumber = 0;
-let answer = 0;
-let operator = "";
 const calculator = document.querySelector(".container");
+const numberButtons = document.querySelectorAll(".input");
+const display = document.querySelector(".display");
+const equalsButton = document.querySelector(".equals");
 let add = (firstNumber, secondNumber) => {
   let answer = firstNumber + secondNumber;
   return answer;
@@ -19,29 +19,57 @@ let divide = (firstNumber, secondNumber) => {
   return answer;
 };
 
-let operate = (firstNumber, operator, secondNumber, e) => {
+let operate = (firstNumber, operator, secondNumber) => {
+  let poo = "";
   switch (operator) {
     case "+":
-      add(firstNumber, secondNumber);
+      poo = add(parseInt(firstNumber), parseInt(secondNumber));
       break;
     case "-":
-      subtract(firstNumber, secondNumber);
+      poo = subtract(parseInt(firstNumber), parseInt(secondNumber));
       break;
     case "*":
-      multiply(firstNumber, secondNumber);
+      poo = multiply(parseInt(firstNumber), parseInt(secondNumber));
       break;
     case "/":
-      divide(firstNumber, secondNumber);
+      poo = divide(parseInt(firstNumber), parseInt(secondNumber));
       break;
   }
+  return poo;
 };
+let inputArr = [];
 
-let numberButtons = document.querySelectorAll(".input");
-numberButtons.forEach((number) => {
-  let display = document.getElementsByClassName("display");
+numberButtons.forEach((number, index) => {
   number.addEventListener("click", (e) => {
-    display[0].innerHTML = e.target.textContent;
-    console.log(display);
+    inputArr.push(e.target.textContent);
+    display.innerHTML = e.target.textContent;
   });
 });
-console.log(numberButtons);
+
+//takes string of first and 2nd numbers and turns them into a real number
+let findOperator = () => {
+  let operatorList = ["+", "-", "*", "/"];
+  let firstNumber = "";
+  let operator = "";
+  let secondNumber = "";
+  let operatorIndex;
+  for (let i = 0; i < inputArr.length; i++) {
+    if (operatorList.includes(inputArr[i])) {
+      operatorIndex = i;
+      operator = inputArr[i];
+    }
+  }
+  for (let i = 0; i < operatorIndex; i++) {
+    firstNumber += inputArr[i];
+  }
+  for (let i = operatorIndex + 1; i < inputArr.length; i++) {
+    secondNumber += inputArr[i];
+  }
+  console.log(inputArr);
+  //operate(firstNumber, operator, secondNumber);
+};
+
+// take numbers that were clicked on and separate them into first number operator and 2nd
+equalsButton.addEventListener("click", findOperator);
+// let x = operate(1, "+", 2);
+// console.log(x);
